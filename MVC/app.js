acdc6617922722bd971d906000d4184ab00caf48
kiran myadaram <myadaramsaikiran@gmail.com>
2024-04-23 00:02:27 +0530

    mvc added

---
+++ MVC/app.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const errorController = require("./controllers/error");
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
---
+++ MVC/routes/admin.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const productsController = require("../controllers/products");
+
+const products = [];
+
+// /admin/add-product => GET
+router.get("/add-product", productsController.getAddProduct);
+
+// /admin/add-product => POST
+router.post("/add-product", productsController.postAddproducts);
+
+module.exports = router;
