be2c46fe6da340b706b6ed665cf55aae74522eb1
kiran myadaram <myadaramsaikiran@gmail.com>
2024-06-21 12:57:03 +0530

    socket.io added

---
+++ websocket-node/server/app.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+
+const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
+const { Socket } = require("net");
+const { log } = require("console");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, uuidv4());
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
+
+const uri =
+  "mongodb+srv://myadaramsaikiran:b88cFroQ51GduA9K@cluster0.s1mnznz.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0 ";
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    writeConcern: { w: "majority" },
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+
+    // Start the server
+    const server = app.listen(8080, () => {
+      console.log("Server is running on port 8080");
+    });
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client Connected");
+    });
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB Atlas:", err);
+  });
